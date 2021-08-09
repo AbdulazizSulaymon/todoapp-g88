85c9fc341ea5407b51bd52be5f4687774a59e8f4
AbdulazizSulaymon <djshahrick@gmail.com>
2021-08-09 10:01:41 +0500

    dev tool

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,13 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import reducer from "./reducer";
 
-const initialStore = {
-    value: "123",
-    tasks: [
-        { title: "task 1" },
-        { title: "task 2" },
-        { title: "task 3" },
-    ]
-}
+const middleware = [thunk];
 
-const reducer = (state = initialStore, action) => {
-    switch (action.type) {
-        case "SET_VALUE":
-            return { ...state, value: action.payload };
-
-        case "ADD_TASK":
-            if (action.payload.trim() === "") return state;
-
-            return {
-                ...state,
-                value: "",
-                tasks: [...state.tasks, { title: action.payload }]
-            }
-
-        case "DELETE_TASK":
-            const tasks = [...state.tasks];
-            tasks.splice(action.payload, 1);
-
-            return { ...state, tasks: tasks };
-
-        default: return state;
-    }
-}
-
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
